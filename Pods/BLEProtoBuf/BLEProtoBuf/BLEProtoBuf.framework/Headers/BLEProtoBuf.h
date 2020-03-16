7c318bae3d8d60ea3a8e4109f69c37460e7a1663
Johan Sellström <johan@sellstrom.me>
2020-03-16 14:53:12 +0100

    add pods

---
+++ Pods/BLEProtoBuf/BLEProtoBuf/BLEProtoBuf.framework/Headers/BLEProtoBuf.h
@@ -0,0 +1,35 @@
+//
+//  BLEProtocBuff.h
+//  BLEProtocBuff
+//
+//  Created by A$CE on 2018/10/16.
+//  Copyright © 2018年 Iwown. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BLEProtocBuff.
+FOUNDATION_EXPORT double BLEProtocBuffVersionNumber;
+
+//! Project version string for BLEProtocBuff.
+FOUNDATION_EXPORT const unsigned char BLEProtocBuffVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BLEProtoBuf/PublicHeader.h>
+
+
+#import <BLEProtoBuf/SolsticePb.h>
+#import <BLEProtoBuf/QuinoxPb.h>
+#import <BLEProtoBuf/BLEProtocBuff.h>
+#import <BLEProtoBuf/PBModel.h>
+#import <BLEProtoBuf/PBDataHealth.h>
+
+
+/*****************************************************************************
+ ****
+ ** BLEProtoBuf.h                            *   * * *
+ ** Version : 0.2.7                          *   *
+ ** Date : 2019-08-02                        * * * * *
+ **                                              *   *
+ **                                          * * *   *
+ ****
+ ***************************************************************************/
